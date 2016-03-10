5bf667d11186040cf82e084b431d3d6b5b381999
kris <Krisscholte@hotmail.com>
2016-03-10 15:58:18 +0100

    Leader beginnetje KillerByte superclass

--- Leader.java
+++ Leader.java
@@ -1,13 +1,19 @@
 package EHI1VSo_1_KillerBytes;
 
+import robocode.ScannedRobotEvent;
 import robocode.TeamRobot;
 
 /**
  * Created by kris on 9-3-16.
  */
-public class Leader extends TeamRobot {
+public class Leader extends killerByte{
     public void run(){
         while(true){
+            super.goCrazy();
         }
+    }
+    //Leader has no specific tasks, because another robot should be able to take over the position
+    public void onScannedRobot(ScannedRobotEvent TargetTank){
+        super.chase(TargetTank);
     }
 }
