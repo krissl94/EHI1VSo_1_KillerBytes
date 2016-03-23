0de075deac49480d89ef83a4e28d1472d583a94b
kris <Krisscholte@hotmail.com>
2016-03-23 12:14:51 +0100

    MoveTo coordinates

--- Droid.java
+++ Droid.java
@@ -8,7 +8,8 @@ import robocode.TeamRobot;
 public class Droid extends KillerByte implements robocode.Droid {
     public void run(){
         while(true){
-            moveTo(10,10);
+            goTo(170,450);
+            execute();
             //enemyStats.getEnemies().get(enemyStats.getTargetName()).getRecordedPositions().get(0)[0], enemyStats.getEnemies().get(enemyStats.getTargetName()).getRecordedPositions().get(0)[1]
         }
     }
